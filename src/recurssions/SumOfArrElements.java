c6ab9b69e760d89e29a5255bc2177fd7466f450f
Riship Gupta <guptariship@gmail.com>
2022-10-13 07:16:30 +0530

    Refactor SumOfArrElements.java..using startIndex to avoid making copies of array while traversing through it

--- src/recurssions/SumOfArrElements.java
+++ src/recurssions/SumOfArrElements.java
@@ -1,21 +1,15 @@
 package recurssions;
 
 public class SumOfArrElements {
-    public static int sum(int input[]) {
-        if (input.length == 1) {
-            return input[0];
+    public static int sum(int input[],int startIndex) {
+        if(startIndex == input.length){
+            return 0;
         }
-        int smallArr[] = new int[input.length - 1];
-        //problem with this solution is that we are making too many copies of smallArr
-        for (int i = 1; i < input.length; i++) {
-            smallArr[i - 1] = input[i];
-        }
-        int result = input[0] + sum(smallArr);
-        return result;
+        return input[startIndex]+sum(input,startIndex+1);
     }
 
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 5};
-        System.out.println(sum(arr));
+        System.out.println(sum(arr,0));
     }
 }
