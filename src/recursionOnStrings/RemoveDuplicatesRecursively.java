fe57d3547f5fb62deb9eff9973b78261541aae15
Riship Gupta <guptariship@gmail.com>
2022-10-14 00:27:00 +0530

    Add RemoveDuplicatesRecursively.java

---
+++ src/recursionOnStrings/RemoveDuplicatesRecursively.java
@@ -0,0 +1,21 @@
+package recursionOnStrings;
+
+//Given a string S, remove consecutive duplicates from it recursively.
+public class RemoveDuplicatesRecursively {
+    public static String removeDuplicates(String s) {
+        if(s.length()<=1){
+            return s;
+        }
+        if (s.charAt(0) == s.charAt(1)) {
+            String smallStr = removeDuplicates(s.charAt(1)+s.substring(2));
+            return smallStr;
+        }else{
+            String smallStr = removeDuplicates(s.substring(1));
+            return s.charAt(0) + smallStr;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates("xxxyyyzzzzwxxwwy"));
+    }
+}
