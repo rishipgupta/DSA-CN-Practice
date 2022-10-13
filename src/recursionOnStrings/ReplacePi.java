83ade699a7f9f325ac37dcbc6333361af01f135a
Riship Gupta <guptariship@gmail.com>
2022-10-13 16:07:15 +0530

    Add ReplacePi.java

--- src/recursionOnStrings/ReplacePi.java
+++ src/recursionOnStrings/ReplacePi.java
@@ -1,2 +1,26 @@
-package recursionOnStrings;public class ReplacePi {
+package recursionOnStrings;
+
+public class ReplacePi {
+    public static String replacePi(String s) {
+       //base case
+        if (s.length() <= 1) {
+            return s;
+        }
+        if (s.charAt(0) == 'p' && s.charAt(1) == 'i') {
+            //run recursion on string of length n-2;\
+            //Induction Hypothesis
+            String smallStr = replacePi(s.substring(2));
+            //induction step;
+            return "3.14" + smallStr;
+
+        } else {
+            //run recursion on string of length n-1
+            String smallStr = replacePi(s.substring(1));
+            return s.charAt(0) + smallStr;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(replacePi("piabcpi"));
+    }
 }
