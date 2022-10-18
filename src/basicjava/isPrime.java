a8bf73d73da184db5e28ff308df47436f0145445
Riship Gupta <guptariship@gmail.com>
2022-10-18 21:16:19 +0530

    Refactored isPrime.java

--- src/basicjava/isPrime.java
+++ src/basicjava/isPrime.java
@@ -6,14 +6,18 @@ public class isPrime {
         if(n==2){
             isPrimeNum = true;
         }
-        for(int i=2;i<n/2;i++){
+        for(int i=2;i<Math.sqrt(n);i++){
             if(n%i==0){
                 isPrimeNum = false;
             }
         }
-        System.out.println(isPrimeNum);
+        if(isPrimeNum){
+        System.out.println("Prime Number");
+        }else{
+            System.out.println("Not Prime");
+        }
     }
     public static void main(String[] args) {
-        isPrime(10);
+        isPrime(11);
     }
 }
