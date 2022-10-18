748186587ba2d9414087509193201ac16fb86726
Riship Gupta <guptariship@gmail.com>
2022-10-19 01:14:49 +0530

    update git ignore

---
+++ src/loops_conditionals/FindLargestNumber.java
@@ -0,0 +1,18 @@
+package loops_conditionals;
+import java.util.Scanner;
+public class FindLargestNumber {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int a = num.nextInt();
+        int b = num.nextInt();
+        int c = num.nextInt();
+        int max = a;
+        if(b>max){
+            max = b;
+        }
+        if(c>max){
+            max = c;
+        }
+        System.out.println(max);
+    }
+}
