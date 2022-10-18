7e9f48289251eba82e3a63d8849407e2efd3cada
Riship Gupta <guptariship@gmail.com>
2022-10-19 01:56:39 +0530

    Add CheckCase.java

---
+++ src/conditionalsAndLoops/CheckCase.java
@@ -0,0 +1,14 @@
+package conditionalsAndLoops;
+import java.util.Scanner;
+import java.lang.*;
+public class CheckCase {
+    public static void main(String[] args) {
+    Scanner c = new Scanner(System.in);
+    char ch = c.next().charAt(0);
+    if(Character.isUpperCase(ch)){
+    System.out.println("is UpperCase");
+    }else{
+        System.out.println("is lower case");
+    }
+    }
+}
