8d12e85ceb480523ef6db9254017c34f4cb21f2b
Riship Gupta <guptariship@gmail.com>
2022-10-19 18:46:53 +0530

    Handle edge cases of numbers equal to multiples of 10

--- src/conditionalsAndLoops/CountingOccurrences.java
+++ src/conditionalsAndLoops/CountingOccurrences.java
@@ -4,6 +4,7 @@ public class CountingOccurrences {
     public static void countOccurence(int n, int x){
         int originalNum = n;
         int count = 0;
+        //For numbers greater than or equal to 10
         while(n/10 != 0){
         int num = n%10;
             n = (n-num)/10;
@@ -11,6 +12,7 @@ public class CountingOccurrences {
                 count++;
             }
         }
+        //For numbers less than 10
         if(n/10==0){
             if(n==x){
                 count++;
@@ -20,6 +22,6 @@ public class CountingOccurrences {
     }
 
     public static void main(String[] args) {
-        countOccurence(100,0);
+        countOccurence(10,8);
     }
 }
