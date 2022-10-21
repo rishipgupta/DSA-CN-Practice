8e72037b3bc2291661812fa35a75a34796197b0d
Riship Gupta <guptariship@gmail.com>
2022-10-21 06:52:27 +0530

    Add PrintArmStrongNumbers.java

---
+++ src/conditionalsAndLoops/PrintArmStrongNumbers.java
@@ -0,0 +1,27 @@
+package conditionalsAndLoops;
+
+public class PrintArmStrongNumbers {
+    public static boolean isArmStrong(int n) {
+        int a = n;
+        int sum = 0;
+        while (a > 0) {
+            int rem = a % 10;
+            a = a / 10;
+            sum = sum + (rem * rem * rem);
+        }
+        return sum == n;
+    }
+
+    static void printThreeDigitArmStrongNum() {
+        for (int i = 100; i < 1000; i++) {
+            if (isArmStrong(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(isArmStrong(233));
+        printThreeDigitArmStrongNum();
+    }
+}
