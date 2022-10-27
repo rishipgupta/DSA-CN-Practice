437b770fefc9b7ba5763f740d8f5bf95c626e56b
Riship Gupta <guptariship@gmail.com>
2022-10-27 19:50:18 +0530

    Constructor Overloading

--- src/oop/Main.java
+++ src/oop/Main.java
@@ -7,11 +7,20 @@ public class Main {
     public static void main(String[] args) {
         Student s1 = new Student(12, 83.5F, "Rahul");
         Student s2 = new Student(12, 80.2F, "Dev");
-        System.out.println(s1.name);
-        System.out.println(s2.name);
-        s1.changeName("Harry");
-        System.out.println(s1.name);
-        System.out.println(s2.name);
+//        System.out.println(s1.name);
+//        System.out.println(s2.name);
+//        s1.changeName("Harry");
+//        System.out.println(s1.name);
+//        System.out.println(s2.name);
+
+
+        //While Creating s3 the constructor with only rollNo and name parameter will be called
+        Student s3 = new Student(54, "Aman");
+        System.out.println(s3.rollNo);
+        System.out.println(s3.name);
+        //marks will give default value for floats
+        System.out.println(s3.marks);
+
     }
 }
 
@@ -21,6 +30,13 @@ class Student{
     String name;
 
     void changeName(String name){
+        this.name = name;
+    }
+
+    //Constructor Overloading: Creating multiple constructors for different scenerios
+
+    Student(int rollNo, String name){
+        this.rollNo = rollNo;
         this.name = name;
     }
 
