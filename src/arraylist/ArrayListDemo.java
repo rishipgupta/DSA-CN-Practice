fd1f04e25c0fce4e438d5a99ffb869f57a4d99e4
Riship Gupta <guptariship@gmail.com>
2022-10-30 13:37:52 +0530

    Set capacity of Array List

--- src/arraylist/ArrayListDemo.java
+++ src/arraylist/ArrayListDemo.java
@@ -13,5 +13,10 @@ public class ArrayListDemo {
         arr.add(1, 80);
         System.out.println(arr.size());
         System.out.println(arr.get(1));
+
+        //To specify default capacity of ArrayList you can do that
+        ArrayList<String> strArrList = new ArrayList<>(20);
+        //Above code shows out initial Capacity is set to 20;
+        //By default it is of capacity = 10;
     }
 }
