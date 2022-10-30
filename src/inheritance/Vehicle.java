b9afbf1801e6534198884e5f22fa4a9132810a64
Riship Gupta <guptariship@gmail.com>
2022-10-30 06:20:50 +0530

    Abstract method and abstract class

--- src/inheritance/Vehicle.java
+++ src/inheritance/Vehicle.java
@@ -1,6 +1,6 @@
 package inheritance;
 
-public class Vehicle {
+public abstract class Vehicle {
     String color;
     protected int maxSpeed;
 
@@ -10,4 +10,10 @@ public class Vehicle {
     public void print() {
         System.out.println("Color of Vehicle is " + color + " and max speed is " + maxSpeed);
     }
+
+    //abstract method is used when we don't know what the body of function would be.
+    //When the output varies from one child class to other
+    //Car class would have different output.
+    //Bike class would have different output
+    public abstract boolean isMotorized();
 }
