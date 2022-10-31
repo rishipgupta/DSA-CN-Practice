6587c9d7be7ee65b4381c55d98a8a3933f0afbb5
Riship Gupta <guptariship@gmail.com>
2022-10-31 11:29:39 +0530

    Create Simple Linked List without generics

---
+++ src/linkedlist/LinkedListUse.java
@@ -0,0 +1,9 @@
+package linkedlist;
+
+public class LinkedListUse {
+    public static void main(String[] args) {
+        Node n1 = new Node(10);
+        System.out.println(n1.data);
+        System.out.println(n1.next);
+    }
+}
